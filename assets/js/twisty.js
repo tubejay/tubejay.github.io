b41bbe050d5ba42d851f4ae1445c0d86aa13edf7
tubejay <92285528+tubejay@users.noreply.github.com>
2023-03-26 21:07:22 +0900

    test twisty js

--- assets/js/twisty.js
+++ assets/js/twisty.js
@@ -1,7 +1,6 @@
-const setAttributes = (el,attrs) => {
-    Object.entries(attrs).forEach(
-        (key,value) => el.setAttribute(key,value)
-    );
+const setAttr = (el,attr) => {
+    const [key,value] = attr;
+    el.setAttribute(key,value);
 };
 
 const attrs = {
@@ -11,5 +10,9 @@ const attrs = {
 };
 
 document.querySelectorAll("twisty-player").forEach(
-    el => setAttributes(el,attrs)
+    // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+    el => Object.entries(attrs).forEach(
+        // https://stackoverflow.com/a/12274782
+        attr => setAttr(el,attr)
+    )
 );
