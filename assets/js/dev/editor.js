b01c9b7dc43c7d401065018d6eedf39b9c86b605
tubejay <92285528+tubejay@users.noreply.github.com>
2023-04-09 21:24:58 +0900

    test log

--- assets/js/dev/editor.js
+++ assets/js/dev/editor.js
@@ -4,7 +4,7 @@ let queryText;
 
 queryText    = "#test";
 const testEl = document.querySelector(queryText);
-test.append("test start");
+testEl.append("test start");
 
 
 
@@ -13,23 +13,31 @@ test.append("test start");
 queryText        = "div";
 const loader     = document.createElement(queryText);
 testEl.appendChild(loader);
+testEl.append("append loader");
 
 const src        = '"https://codebeautify.org/css-to-scss-converter"';
 loader.innerHTML = '<iframe src=' + src + ' width="100px" height="100px"></iframe>';
 
 queryText        = "iframe";
 const ifEl       = document.querySelector(queryText);
+testEl.append("select ifEl");
 
 
 
 // https://stackoverflow.com/a/7570527
-const ifDoc = ifEl.contentWindow.document;
+const ifDoc = ifEl.contentDocument;
+testEl.append("select ifDoc");
 
 // https://ajaxorg.github.io/ace-api-docs/classes/Ace.EditSession.html
 queryText   = ".EditSession";
 const edEl  = ifDoc.body.querySelector(queryText);
+testEl.append("select edEl");
 const edDoc = edEl.doc
+testEl.append("select edDoc");
 
 docEl.forEach(
     line => test.append(line)
 );
+
+testEl.removeChild(loader);
+testEl.append("remove loader");
