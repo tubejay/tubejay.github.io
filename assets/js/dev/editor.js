5349221d9bfc2022f05ce4db7d6da2fb0157c190
tubejay <92285528+tubejay@users.noreply.github.com>
2023-04-25 14:14:15 +0900

    animate iteration

--- assets/js/dev/editor.js
+++ assets/js/dev/editor.js
@@ -1201,10 +1201,10 @@ const convertButtonListener = () => {
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
     click : {convertButtonAnimate} ,
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/animationiteration_event
-    animationIteration : {convertButtonConvert} ,
+    animationiteration : {convertButtonConvert} ,
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/animationiteration_event#browser_compatibility
     // https://stackoverflow.com/a/19132952
-    webkitAnimationIteration : {convertButtonConvert}
+    WebkitAnimationIteration : {convertButtonConvert}
   };
 
   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
