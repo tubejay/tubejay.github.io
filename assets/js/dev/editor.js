6fa085287448d05bf5ffbf6b58699162dfb5169e
tubejay <92285528+tubejay@users.noreply.github.com>
2023-04-28 07:52:59 +0900

    table error log

--- assets/js/dev/editor.js
+++ assets/js/dev/editor.js
@@ -59,12 +59,16 @@ const cellsToRow = cellArr => {
   return tr;
 };
 const createTable = columnArr => {
+  testLine( "createTable" , false );
   // table
+  testLine( "- table" , false );
   const table = document.createElement("table");
   // tbody
+  testLine( "- tbody" , false );
   const tbody = document.createElement("tbody");
   table.appendChild(tbody);
   // columns -> rows
+  testLine( "- rowArr" , false );
   const rowArr = columnsToRows(columnArr).map(cellsToRow);
   // add rows
   rowArr.forEach( row =>
